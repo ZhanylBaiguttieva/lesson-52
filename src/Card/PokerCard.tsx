a7de515aa451835f7949fc164e5633dc9e495ba9
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-10-23 15:06:18 +0600

    Created component PokerCard.tsx

--- src/Card/PokerCard.tsx
+++ src/Card/PokerCard.tsx
@@ -4,12 +4,15 @@ interface Props {
     rank: string;
     suit: string;
 }
-const PokerCard: React.FC<Props> = ({rank},{suit}) => {
+const PokerCard: React.FC<Props> = ({rank,suit}) => {
     return (
-        <div className="cardName">
-
-        </div>
-    )
+      <div className="playingCards faceImages">
+        <span className={"card " + "rank-" + rank + " " + suit}>
+          <span className="rank">{rank}</span>
+          <span className="suit">{suit}</span>
+        </span>
+      </div>
+    );
 };
 
-export  default PokerCard;
+export default PokerCard;
