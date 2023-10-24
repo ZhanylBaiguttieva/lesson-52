531c4db7c38995dfe59dced07249cb0a21722107
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-10-24 18:13:34 +0600

    Added new Pokerhand();

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Card from "./lib/card";
 import CardDeck from "./lib/cardDeck";
 import {useState} from "react";
+import PokerHand from "./lib/pokerHand";
 
 function App() {
     const [cards, setCards] = useState<Card[]>([]);
@@ -12,10 +13,15 @@ function App() {
         setCards(cardDeck.getCards(5));
     }
 
+    const pokerHand = new PokerHand(cards);
+
     return (
         <div className="App">
             {cards.length !== 0 &&
-                cards.map((card) => <PokerCard rank={card.rank} suit={card.suit}></PokerCard>)}
+                cards.map(card => <PokerCard rank={card.rank} suit={card.suit}></PokerCard>)}
+            <div>
+                {pokerHand.getOutcome()}
+            </div>
             <div>
                 <button onClick={deal}>Deal cards</button>
             </div>
