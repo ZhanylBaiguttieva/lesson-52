4b37411473f00f55fb12e71f67a475c4aa958407
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-10-24 18:15:44 +0600

    added getSuitImg in <div>

--- src/Card/PokerCard.tsx
+++ src/Card/PokerCard.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import CardDeck from "../lib/cardDeck";
 
 interface Props {
     rank: string;
     suit: string;
 }
 const PokerCard: React.FC<Props> = ({rank,suit}) => {
+    const cardDeck = new CardDeck();
     return (
       <div className="playingCards faceImages">
-        <span className={"card " + "rank-" + rank + " " + suit}>
+        <span className={"card " + "rank-" + rank.toLowerCase() + " " + suit}>
           <span className="rank">{rank}</span>
-          <span className="suit">{suit}</span>
+          <span className="suit">{cardDeck.getSuitImg(suit)}</span>
         </span>
       </div>
     );
